1c644d0788ff3357bfa373836dbdc0a525a95a73
popunbom <fantom0779@gmail.com>
2019-01-08 11:09:33 +0900

    地図表示アプリを Flask 上に移行 & Leaflet を CDN 利用に変更

--- app/app.py
+++ app/app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    # return "Hello, world"
     return render_template('index.html')
+
+@app.route("/map")
+def show_map():
+    return render_template('map.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
