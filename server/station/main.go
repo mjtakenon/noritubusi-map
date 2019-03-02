9a787d29790723d440597c808b5624b77aa8b685
rain9525 <rain9525@users.noreply.github.com>
2019-03-02 14:31:27 +0900

    Add method that get station infomation in range

--- server/station/main.go
+++ server/station/main.go
@@ -8,11 +8,26 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-type StationData struct {
+type stationInfo struct {
+	Id                  int64  `db:"id"`
+	Name                string `db:"station_name"`
+	Latitude            string `db:"lat"`
+	Longitude           string `db:"long"`
+	Company             string `db:"operation_company"`
+	ServiceProviderType int64  `db:"service_provider_type"`
+	RailwayName         string `db:"railway_line_name"`
+	RailwayType         int64  `db:"railway_type"`
+}
+
+func (s stationInfo) String() string {
+	return fmt.Sprintf("[%4d]%s, (%s,%s), %s, %d, %s, %d", s.Id, s.Name, s.Latitude, s.Longitude, s.Company, s.ServiceProviderType, s.RailwayName, s.RailwayType)
+}
+
+type StationDB struct {
 	DB *sqlx.DB
 }
 
-func (s StationData) New(userName, password, address, dbName string) error {
+func (s *StationDB) New(userName, password, address, dbName string) error {
 	//userName:password@protocol(adress)/dbName
 	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", userName, password, address, dbName))
 
@@ -23,4 +38,16 @@ func (s StationData) New(userName, password, address, dbName string) error {
 
 	s.DB = db
 	return nil
+}
+
+func (s *StationDB) getStationInfoInRange(beginLat, beginLong, endLat, endLong string) ([]stationInfo, error) {
+	query := `select id,station_name,X(center_latlong) AS 'lat',Y(center_latlong) AS 'long',operation_company,service_provider_type,railway_line_name,railway_type from stations where MBRContains(GeomFromText(CONCAT("LINESTRING(",?," ",?,",",?," ", ?,")")),center_latlong)`
+
+	infos := []stationInfo{}
+	err := s.DB.Select(&infos, query, beginLat, beginLong, endLat, endLong)
+	if err != nil {
+		return nil, err
+	}
+
+	return infos, nil
 }
--- server/station/main_test.go
+++ server/station/main_test.go
@@ -1,6 +1,8 @@
 package station
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -48,11 +50,101 @@ func TestStationData_New(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := StationData{
+			s := StationDB{
 				DB: tt.fields.DB,
 			}
 			if err := s.New(tt.args.userName, tt.args.password, tt.args.address, tt.args.dbName); (err != nil) != tt.wantErr {
 				t.Errorf("StationData.New() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestStationDB_getStationInfoInRange(t *testing.T) {
+	type fields struct {
+		DB *sqlx.DB
+	}
+
+	//DBセットアップ
+	userName := "user"
+	password := "password"
+	address := "localhost:3314"
+	dbName := "noritubusi_map"
+	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", userName, password, address, dbName))
+
+	if err != nil {
+		t.Errorf("DB Connection Error:%v", err)
+		return
+	}
+	type args struct {
+		beginLat  string
+		beginLong string
+		endLat    string
+		endLong   string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []stationInfo
+		wantErr bool
+	}{
+		{
+			name:   "near Hamamatsu Station",
+			fields: fields{DB: db},
+			args: args{
+				beginLat:  "34.705549",
+				beginLong: "137.729265",
+				endLat:    "34.702267",
+				endLong:   "137.741667",
+			},
+			want: []stationInfo{
+				{
+					Id:                  437,
+					Name:                "新浜松",
+					Latitude:            "34.70341",
+					Longitude:           "137.73246",
+					Company:             "遠州鉄道",
+					ServiceProviderType: 4,
+					RailwayName:         "鉄道線",
+					RailwayType:         12,
+				},
+				{
+					Id:                  5796,
+					Name:                "浜松",
+					Latitude:            "34.70376",
+					Longitude:           "137.7353775",
+					Company:             "東海旅客鉄道",
+					ServiceProviderType: 1,
+					RailwayName:         "東海道新幹線",
+					RailwayType:         11,
+				},
+				{
+					Id:                  5871,
+					Name:                "浜松",
+					Latitude:            "34.70406",
+					Longitude:           "137.7351175",
+					Company:             "東海旅客鉄道",
+					ServiceProviderType: 2,
+					RailwayName:         "東海道線",
+					RailwayType:         11,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &StationDB{
+				DB: tt.fields.DB,
+			}
+			got, err := s.getStationInfoInRange(tt.args.beginLat, tt.args.beginLong, tt.args.endLat, tt.args.endLong)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StationDB.getStationInfoInRange() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StationDB.getStationInfoInRange() = %v, want %v", got, tt.want)
 			}
 		})
 	}
