9ff7f7bfb305f1a1b54cd48dbfbc98a73a40d625
popunbom <fantom0779@gmail.com>
2019-03-03 14:15:49 +0900

    不要なファイルを削除 ＆ `go run main.go` で実行するスタイルに変更

--- config/ENV.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export PY_REQUIREMENTS_PATH='config/server/requirements.txt'
-export PY_FLASK_APP_PATH='app/app.py'
--- config/server/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source /config/ENV.sh
-
-# pip3: Install from requirements.txt
-pip3 install -r $PY_REQUIREMENTS_PATH
-
-# Run Flask server
-python3 $PY_FLASK_APP_PATH
