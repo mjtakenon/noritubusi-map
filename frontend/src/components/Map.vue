e3b87064d7679643775227731e50263eb6a1b21f
popunbom <fantom0779@gmail.com>
2019-03-04 16:32:29 +0900

    マーカー表示ができるようにした

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -11,13 +11,15 @@
       @update:bounds="boundsUpdated"
     >
       <l-tile-layer :url="url"></l-tile-layer>
+      <!-- <l-marker v-for="(m, i) in markers" :key="i" :lat-lng="m.latlong"></l-marker> -->
+      <TMarker v-for="(m, i) in markers" :key="i" :lat-long="m"/>
     </l-map>
     <v-toolbar class="float-toolbar" dense floating>
       <v-btn icon>
         <v-icon>search</v-icon>
       </v-btn>
       <v-text-field clearable label="駅名を入力" single-line></v-text-field>
-      <v-btn icon>
+      <v-btn icon @click="getCurrentRect">
         <v-icon>my_location</v-icon>
       </v-btn>
     </v-toolbar>
@@ -26,20 +28,25 @@
 
 <script>
 // Module: vue2-leaflet
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { LMap, LTileLayer } from "vue2-leaflet";
+import TMarker from "./Marker";
 import "leaflet/dist/leaflet.css";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    TMarker
   },
   data() {
     return {
-      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      // url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      url: "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
       zoom: 14,
-      center: [35.680446, 139.761801],
+      center: {
+        lat: 35.680446,
+        lng: 139.761801
+      },
       bounds: {
         _southWest: {
           lat: 35.63532680480169,
@@ -49,7 +56,8 @@ export default {
           lat: 35.691113860493594,
           lng: 139.79489050805572
         }
-      }
+      },
+      markers: []
     };
   },
   methods: {
@@ -61,6 +69,30 @@ export default {
     },
     boundsUpdated(bounds) {
       this.bounds = bounds;
+    },
+    getCurrentRect() {
+      axios
+        .get(`http://${window.location.hostname}:1323/stations`, {
+          params: {
+            begin_latitude: this.bounds._northEast.lat,
+            begin_longitude: this.bounds._northEast.lng,
+            end_latitude: this.bounds._southWest.lat,
+            end_longitude: this.bounds._southWest.lng
+          }
+        })
+        .then(resp => {
+          console.log("Axios SUCCESS!");
+          console.log(`response: ${resp}`);
+          console.log(`status: ${resp.status}`);
+          console.log(resp.data);
+          this.markers = resp.data.map(elem => ({
+            lat: elem.latitude,
+            lng: elem.longitude
+          }));
+        })
+        .catch(err => {
+          console.log(`Axios ERROR!: ${err}`);
+        });
     }
   }
 };
