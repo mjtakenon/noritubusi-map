0f61e344a3115b2e0320d555b10199cbce53b8bd
popunbom <fantom0779@gmail.com>
2019-03-08 15:14:15 +0900

    LInted: main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,30 +11,28 @@ Vue.config.productionTip = false;
 // Module: vue2-leaflet
 
 // Icon fix
-import {
-  Icon
-} from "leaflet";
+import { Icon } from "leaflet";
 delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions( {
-  iconRetinaUrl: require( "leaflet/dist/images/marker-icon-2x.png" ),
-  iconUrl: require( "leaflet/dist/images/marker-icon.png" ),
-  shadowUrl: require( "leaflet/dist/images/marker-shadow.png" )
-} );
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
 
 // Import css
 import "./stylesheet/main.css";
 
 /* eslint-disable no-new */
-Vue.use( Vuetify, {
+Vue.use(Vuetify, {
   theme: {
     primary: "#378640"
   }
-} );
-const vm = new Vue( {
+});
+const vm = new Vue({
   el: "#app",
   router,
   components: {
     App
   },
   template: "<App/>"
-} );
+});
