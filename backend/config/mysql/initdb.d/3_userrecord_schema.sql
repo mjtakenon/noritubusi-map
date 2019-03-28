1220bec9ae44108d7f5178661d4f5f8dbab5a249
rain9525 <rain9525@users.noreply.github.com>
2019-03-28 14:46:13 +0900

    add index to create_time

--- backend/config/mysql/initdb.d/3_userrecord_schema.sql
+++ backend/config/mysql/initdb.d/3_userrecord_schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `user_records` (
   `user_id`             VARCHAR(128) NOT NULL,
   `station_id`         INT UNSIGNED NOT NULL,
   `create_time`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`user_id`),
+  KEY `create_time` (`create_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
