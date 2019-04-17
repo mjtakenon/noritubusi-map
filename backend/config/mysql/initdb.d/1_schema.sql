0e2e7ff7ef702e476ed87b6ff8828bca0f4c26a9
mjtakenon <cs15503@s.inf.shizuoka.ac.jp>
2019-04-17 16:53:50 +0900

    SQL文の生成コードを追加 スキーマを変更

--- backend/config/mysql/initdb.d/1_schema.sql
+++ backend/config/mysql/initdb.d/1_schema.sql
@@ -9,9 +9,6 @@ CREATE TABLE IF NOT EXISTS `buildings` (
 CREATE TABLE IF NOT EXISTS `railways` (
   `id`                     INT NOT NULL AUTO_INCREMENT,
   `name`                   VARCHAR(128) NOT NULL,
-  `type`                   TINYINT UNSIGNED NOT NULL,
-  `company_name`           VARCHAR(128) NOT NULL,
-  `service_provider_type`  TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   INDEX (`company_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -21,8 +18,8 @@ CREATE TABLE IF NOT EXISTS `stations` (
   `name`                   VARCHAR(128) NOT NULL,
   `railway_id`             INT NOT NULL,
   `building_id`            INT NOT NULL,
+  `num_in_railway`         TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  -- FOREIGN KEY(列名) REFERENCES 親テーブル名(親列名)
   FOREIGN KEY (`building_id`) REFERENCES buildings(`id`), 
   FOREIGN KEY (`railway_id`) REFERENCES railways(`id`) ,
   INDEX (`name`)
