c02d17fb4188637cadd30c9bb132a2c654f4604f
mjtakenon <takenon777@gmail.com>
2019-04-20 02:09:13 +0900

    出力したsqlの移動を忘れてました

--- backend/config/mysql/initdb.d/2_seeds.sql
+++ backend/config/mysql/initdb.d/2_seeds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c52c3ebc06dd0e14f48f7d25da9877441fa065d60cc997620c76e997238eac4
-size 2847325
+oid sha256:c15f96261d2421469bee7e8fcbc446237e0ddcf5333d9d7bdcf0f772b0abd235
+size 2797462
