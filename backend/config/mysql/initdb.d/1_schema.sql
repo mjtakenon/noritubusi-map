f5d7df178221ef2f42fe210176256e1c62fc2bbe
mjtakenon <takenon777@gmail.com>
2019-04-23 12:18:21 +0900

    1_schemaのコンフリクトメッセージを消してなかったので修正

--- backend/config/mysql/initdb.d/1_schema.sql
+++ backend/config/mysql/initdb.d/1_schema.sql
@@ -21,13 +21,8 @@ CREATE TABLE IF NOT EXISTS `stations` (
   `building_id`            INT NOT NULL,
   `num_in_railway`         TINYINT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-<<<<<<< a60946f400b50c193829c49d6267e347da3afc5c
   -- FOREIGN KEY(列名) REFERENCES 親テーブル名(親列名)
   FOREIGN KEY (`building_id`) REFERENCES buildings(`id`),
   FOREIGN KEY (`railway_id`) REFERENCES railways(`id`),
-=======
-  FOREIGN KEY (`building_id`) REFERENCES buildings(`id`), 
-  FOREIGN KEY (`railway_id`) REFERENCES railways(`id`) ,
->>>>>>> SQL文の生成コードを追加
   INDEX (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
