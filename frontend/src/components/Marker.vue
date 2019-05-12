78260eeee4a7699b97fe92be5842e956e1b840ff
popunbom <fantom0779@gmail.com>
2019-05-12 22:25:43 +0900

    Edit: Marker.vue - マーカーをデフォルトから円マーカーに変更 - リファクタリング

--- frontend/src/components/Marker.vue
+++ frontend/src/components/Marker.vue
@@ -1,20 +1,27 @@
 <template>
-  <l-marker :lat-lng="data" v-on:click="onClick" @l-add="$event.target.openPopup()">
-    <TPopup :v-show="isVisible" :data="data"></TPopup>
+  <l-marker :lat-lng="markerInfo" v-on:click="onClick" @l-add="$event.target.openPopup()">
+    <l-icon :icon-anchor="markerInfo" :icon-size="[24, 24]" class-name="circle-icon">
+      <!-- <v-icon color="#ff8c00">lens</v-icon> -->
+      <svg viewBox="0 0 24 24">
+        <circle cx="12" cy="12" r="10" stroke="#FFFFFF" stroke-width="2" :fill="fillColor"></circle>
+      </svg>
+    </l-icon>
+    <TPopup :v-show="isVisible" :popupInfo="markerInfo"></TPopup>
   </l-marker>
 </template>
 
 <script>
-import { LMarker } from "vue2-leaflet";
+import { LMarker, LIcon } from "vue2-leaflet";
 import TPopup from "./Popup";
 
 export default {
   components: {
     LMarker,
+    LIcon,
     TPopup
   },
   props: {
-    data: {
+    markerInfo: {
       type: Object,
       required: true
     }
@@ -23,6 +30,19 @@ export default {
     return {
       isVisible: false
     };
+  },
+  computed: {
+    fillColor: function() {
+      var nOfLines = this.markerInfo.lines.length;
+
+      if (nOfLines >= 5) {
+        return "red";
+      } else if (nOfLines >= 2) {
+        return "orange";
+      } else {
+        return "blue";
+      }
+    }
   },
   methods: {
     onClick: function() {
