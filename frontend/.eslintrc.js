949e5f88f1116807033d09b1641a7b5992b86909
popunbom <fantom0779@gmail.com>
2019-12-04 15:32:28 +0900

    Edit: .eslintrc.js - "semi" を false に変更

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,20 +6,25 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        // 文末セミコロンに関するエラー
+        "semi": false,
+        "trailingComma": "es5",
+      },
+    ],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 
-    // 文末セミコロンに関するエラー
-    "semi": ["warn", "always"],
+    // undefined エラー
+    "no-undef": "off",
 
-    // // undefined エラー
-    // "no-undef": "off",
+    // 未使用の変数宣言に関するエラー
+    "no-unused-vars": "off",
 
-    // // 未使用の変数宣言に関するエラー
-    // "no-unused-vars": "off",
-
-    // // Vue: import したが未使用のコンポーネントに対するエラー
-    // "vue/no-unused-components": "off"
+    // Vue: import したが未使用のコンポーネントに対するエラー
+    "vue/no-unused-components": "off"
   },
   parserOptions: {
     parser: "babel-eslint"
