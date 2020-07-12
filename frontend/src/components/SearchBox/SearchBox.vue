809d4d4c522bdd2fab3b876cc73daea839e79e3a
mjtakenon <takenon777@gmail.com>
2020-07-12 22:35:40 +0900

    ユーザー名が表示されないバグを修正
    Co-authored-by: Fumiya ENDOU <fantom0779@gmail.com>

--- frontend/src/components/SearchBox/SearchBox.vue
+++ frontend/src/components/SearchBox/SearchBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-bind:style="style.searchBox">
     <v-toolbar dense>
-      <v-btn icon>
+      <v-btn @click="toggleSidebar" icon>
         <v-icon>fa-bars</v-icon>
       </v-btn>
 
@@ -37,6 +37,31 @@ export default {
         },
       },
     }
+  },
+  // メソッド
+  methods: {
+    toggleSidebar() {
+      this.showSidebar = !this.showSidebar
+    },
+  },
+  // 算出プロパティ
+  computed: {
+    // showSidebar は Vuex 上で状態管理をしているため、
+    // Vue の算出プロパティ機能を使ってゲッター／セッターを用意する
+    // Vuex インスタンスへは this.$store でアクセスできる
+    showSidebar: {
+      get() {
+        // Vuex 上で管理しているデータには $store.getters でアクセスできる
+        // $store.getters['対象データへのパス']
+        return this.$store.getters["Sidebar/isVisible"]
+      },
+      set(value) {
+        // Vuex 上で管理しているデータに関する変更は $store.dispatch で行える
+        // dispatch は非同期処理なので、データ変更を「予約」する形になる
+        // $store.dispatch('対象データへのパス', '変更後の値')
+        this.$store.dispatch("Sidebar/isVisible", value)
+      },
+    },
   },
 }
 </script>
