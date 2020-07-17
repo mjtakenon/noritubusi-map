2fbb8cbad7a1047022fb6d9679c7651163ea31e3
mjtakenon <takenon777@gmail.com>
2020-07-17 23:06:13 +0900

    サジェストに路線名を表示
    Co-authored-by: Fumiya ENDOU <fantom0779@gmail.com>

--- frontend/src/components/SearchBox/SuggestList.vue
+++ frontend/src/components/SearchBox/SuggestList.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <v-list style="max-height: 200px; overflow-y: auto;">
+    <v-list two-line style="max-height: 200px; overflow-y: auto;">
       <v-list-item-group v-model="stations" color="primary">
         <v-list-item v-for="(station, i) in this.stations" :key="i">
-          <v-list-item-icon>
-            <v-icon>train</v-icon>
-          </v-list-item-icon>
+          <v-list-item-avatar style="font-size: 150%">
+            <div v-if="isShinkansen(station.railway_line_name)">🚅</div>
+            <div v-else>🚃</div>
+          </v-list-item-avatar>
           <v-list-item-content>
             <v-list-item-title> {{ station.name }} </v-list-item-title>
+            <v-list-item-subtitle> {{ station.railway_line_name }} </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
@@ -30,7 +32,11 @@ export default {
     }
   },
   // メソッド
-  methods: {},
+  methods: {
+    isShinkansen(railwayName) {
+      return railwayName.indexOf('新幹線') != -1
+    }
+  },
   // 算出プロパティ
   computed: {}
 };
