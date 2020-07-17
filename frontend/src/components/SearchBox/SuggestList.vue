8a1aa0583163be7e0885775d697a0fa402782843
mjtakenon <takenon777@gmail.com>
2020-07-17 23:29:46 +0900

    サジェストをクリックした時にその駅に移動する機能を追加
    Co-authored-by: Fumiya ENDOU <fantom0779@gmail.com>

--- frontend/src/components/SearchBox/SuggestList.vue
+++ frontend/src/components/SearchBox/SuggestList.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-list two-line style="max-height: 200px; overflow-y: auto;">
-      <v-list-item-group v-model="stations" color="primary">
-        <v-list-item v-for="(station, i) in this.stations" :key="i">
+      <v-list-item-group color="primary">
+        <v-list-item v-for="(station, i) in this.stations" :key="i" @click="focusStation(station)">
           <v-list-item-avatar style="font-size: 150%">
             <div v-if="isShinkansen(station.railway_line_name)">🚅</div>
             <div v-else>🚃</div>
@@ -32,9 +32,25 @@ export default {
     }
   },
   // メソッド
+  // {
+  //   "building_id": 2103,
+  //   "station_id": 4635,
+  //   "name": "東京",
+  //   "latitude": "35.681391",
+  //   "longitude": "139.766103",
+  //   "railway_line_name": "JR総武本線",
+  //   "order_in_railway": 1
+  // }
   methods: {
     isShinkansen(railwayName) {
       return railwayName.indexOf('新幹線') != -1
+    },
+    focusStation(station) {
+      this.$store.dispatch("Map/updateCenter", {
+        lat: station.latitude,
+        lng: station.longitude,
+      })
+      return 
     }
   },
   // 算出プロパティ
